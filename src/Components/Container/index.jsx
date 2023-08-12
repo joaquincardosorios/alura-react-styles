b83528a40ce22c66a90373ca73448de335b9de48
Joaquin Rios <joaquin.rios@crecic.cl>
2023-08-11 21:35:45 -0400

    Configuraciones de styled

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,15 +2,28 @@ import React from "react";
 
 import Title from "../Title";
 import Account from "../Account";
+import styled from "styled-components";
+
+const StyleContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyledContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
 
 const Container = () => {
   return (
-    <div className="container">
+    <StyleContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyledContent>
         <Account />
-      </section>
-    </div>
+      </StyledContent>
+    </StyleContainer>
   );
 };
 
