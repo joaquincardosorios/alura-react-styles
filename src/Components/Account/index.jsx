28f2b800c2ef78a53eeb68306bc863b987fa4029
Joaquin Rios <joaquin.rios@crecic.cl>
2023-08-11 21:40:29 -0400

    Propiedades de Herencia

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -5,6 +5,10 @@ import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
 import styled from "styled-components";
 
+const IconoMargin = styled(Icono)`
+  margin-top: "2px";
+`
+
 const Account = () => {
   const [toggleState, untoggle] = useState(true);
 
@@ -28,8 +32,7 @@ const Account = () => {
       </div>
 
       <button className="btn" onClick={toggleHandler}>
-        <Icono
-          style={{ marginTop: "2px" }}
+        <IconoMargin
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
